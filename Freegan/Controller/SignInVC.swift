da62a988f1a2f7059eb99e6d9c3e3506627ef1c6
hammedopejin <hammedopejin@yahoo.com>
2017-08-03 12:07:00 -0700

    Completed setting up Firebase integration

---
+++ Freegan/Controller/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Freegan
+//
+//  Created by Hammed Opejin on 8/3/17.
+//  Copyright © 2017 Hammed Opejin. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
