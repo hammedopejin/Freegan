9f5823309ad138da4f81d425a5a84d9f0f91c4dd
hammedopejin <hammedopejin@yahoo.com>
2017-08-03 19:24:29 -0700

    Sign in ui custom classes complete

---
+++ Freegan/Util/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Freegan
+//
+//  Created by Hammed Opejin on 8/3/17.
+//  Copyright © 2017 Hammed Opejin. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
