4762b317c3f670bfef74372f7817401a98d0346e
hammedopejin <hammedopejin@yahoo.com>
2017-08-08 18:56:29 -0700

    I update the sign-in view to include registering for the first time and user image selection.

--- Freegan/Controller/SignInVC.swift
+++ Freegan/Controller/SignInVC.swift
@@ -10,13 +10,17 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class SignInVC: UIViewController {
+class SignInVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var emailField: FancyField!
     @IBOutlet weak var pwdField: FancyField!
+    @IBOutlet weak var userNameField: FancyField!
+    @IBOutlet weak var userImg: UIImageView!
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -25,7 +29,13 @@ class SignInVC: UIViewController {
             performSegue(withIdentifier: "goToFeed", sender: nil)
         }
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image =  info[UIImagePickerControllerOriginalImage] as? UIImage{
+            userImg.image = image
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
     
     
     func firebaseAuth(_ credential: AuthCredential) {
@@ -41,7 +51,14 @@ class SignInVC: UIViewController {
             }
         })
     }
-
+    
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirbaseDBUser(uid: id, userData: userData)
+        let keychainResult = KeychainWrapper.defaultKeychainWrapper.set(id, forKey: KEY_UID)
+        print("HAMMED: Data saved to keychain \(keychainResult)")
+        performSegue(withIdentifier: "goToFeed", sender: nil)
+    }
+    
     @IBAction func signInTapped(_ sender: Any) {
         if let email = emailField.text, let pwd = pwdField.text {
             Auth.auth().signIn(withEmail: email, password: pwd, completion: { (user, error) in
@@ -67,13 +84,26 @@ class SignInVC: UIViewController {
             })
         }
     }
-    
-    func completeSignIn(id: String, userData: Dictionary<String, String>) {
-        DataService.ds.createFirbaseDBUser(uid: id, userData: userData)
-        let keychainResult = KeychainWrapper.defaultKeychainWrapper.set(id, forKey: KEY_UID)
-        print("HAMMED: Data saved to keychain \(keychainResult)")
-        performSegue(withIdentifier: "goToFeed", sender: nil)
+    @IBAction func registerTapped(_ sender: Any) {
+        if let email = emailField.text, let pwd = pwdField.text {
+                    Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("HAMMED: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("HAMMED: Successfully authenticated with Firebase")
+                            if let user = user {
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
+                            }
+                        }
+                    })
+                }
     }
+    
+    @IBAction func selectImg(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     
 }
 
